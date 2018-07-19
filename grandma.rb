55016eb1c6f0af46e1e483807ea3f132d3d6f1ab
Anita Beauchamp <amadamspr@gmail.com>
2018-07-19 01:17:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(message)
 
   if message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif message != meesage.upcase
+  elsif message != message.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
